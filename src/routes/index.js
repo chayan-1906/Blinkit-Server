abab91eb9f1c3d5df6096cd51a97ff9929079596
System-Edge <padmanabhadas9647@gmail.com>
2024-09-23 11:12:18 +0530

    removed /api from prefix

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,8 @@ import {authRoutes} from "./auth.js";
 import {categoryRoutes, productRoutes} from "./product.js";
 import {orderRoutes} from "./order.js";
 
-const prefix = '/api';
+// const prefix = '/api';
+const prefix = '/';
 
 export const registerRoutes = async (fastify) => {
     fastify.register(authRoutes, {prefix: prefix});
