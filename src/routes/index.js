059280cf9f53c96466bc30e2e255d97f7612f5e8
System-Edge <padmanabhadas9647@gmail.com>
2024-09-26 13:58:54 +0530

    removed .adminjs, api, vercel.json and added a / route in fastify

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,9 @@ const prefix = '/api';
 // const prefix = '/';
 
 export const registerRoutes = async (fastify) => {
+    fastify.get('/', function (request, reply) {
+        return {root: true}
+    });
     fastify.register(authRoutes, {prefix: prefix});
     fastify.register(categoryRoutes, {prefix: prefix});
     fastify.register(productRoutes, {prefix: prefix});
